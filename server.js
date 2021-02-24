43169471dd361ebc34af761a5f51a966a2896551
bluesatyr <shawnevans.music@gmail.com>
2021-02-23 21:43:51 -0500

    create DELETE route and function to remove a candidate by id

--- server.js
+++ server.js
@@ -15,14 +15,22 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the databse.');
 });
 
-/* // GET a single candidate
-db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(row);
-})
-*/
+/// GET a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
@@ -40,15 +48,23 @@ app.get('/api/candidates', (req, res) => {
     });
 });
 
-/*
+
 // DELETE a candidate
-db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result, this, this.changes);
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
 });
-*/
+
 
 /* Create a candidate
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
