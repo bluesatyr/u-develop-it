4cb79670f5aaa352012fc990a881afb0ebb25562
bluesatyr <shawnevans.music@gmail.com>
2021-02-23 21:19:22 -0500

    use database query to retrieve all candidates

--- server.js
+++ server.js
@@ -15,11 +15,61 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the databse.');
 });
 
+/* // GET a single candidate
+db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(row);
+})
+*/
+
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+/*
+// DELETE a candidate
+db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this, this.changes);
+});
+*/
+
+/* Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
+});
+*/
+
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
   });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 });
