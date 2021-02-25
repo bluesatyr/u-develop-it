f79dd903771b9fba0496913712e32af04d5f7be1
bluesatyr <shawnevans.music@gmail.com>
2021-02-25 11:28:34 -0500

    show party affiliation in candidates api route and candidate by id api route

--- server.js
+++ server.js
@@ -18,7 +18,11 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 /// GET a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE candidates.id = ?` // the '?' is a placeholder for the params that follow
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if (err) {
@@ -35,7 +39,10 @@ app.get('/api/candidate/:id', (req, res) => {
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                AS party_name
+                FROM candidates LEFT JOIN parties
+                ON candidates.party_id = parties.id `;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if(err) {
